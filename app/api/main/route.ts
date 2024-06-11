ee538de8067a06415e94310d4a474e1aa080b58b
ShahzodTuraev <turayevshahzodbek@gmail.com>
2024-06-11 15:15:25 +0900

    fix: initialize location of the server data

--- app/api/main/route.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-export default function GET(req: NextApiRequest, res: NextApiResponse) {
-  console.log("server");
-  res.status(200).json({ message: "Hello from Next.js!" });
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import ReactPlayer from "react-player";
 
 export default function Home() {
-  useEffect(() => {
-    const data = fetch("/api/main");
-    console.log(data);
-  }, []);
-
   return (
     <>
       <p>hello</p>
