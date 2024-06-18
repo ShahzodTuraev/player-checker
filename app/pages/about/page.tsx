5f708353aa9c076d4e501b1d244b8aac64ec6450
ShahzodTuraev <turayevshahzodbek@gmail.com>
2024-06-18 15:43:22 +0900

    feat: nfc initialization

---
+++ app/pages/about/page.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div>
+      <p>this page is about page about Shahzod</p>
+      <form action="">
+        <input type="text" />
+        <input type="password" />
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
+
+export default About;
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <div style={{ margin: 50 }}>
+      <textarea name="gdfg" id=""></textarea>
+    </div>
+  );
+};
+
+export default Profile;
