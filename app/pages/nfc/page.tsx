d4a2a4f562075967fa40e4e2dd1a554a9ccdcd1f
ShahzodTuraev <turayevshahzodbek@gmail.com>
2024-06-19 10:10:19 +0900

    feat: nfc scanning event initializing

--- app/pages/nfc/page.tsx
+++ app/pages/nfc/page.tsx
@@ -1,50 +1,13 @@
 "use client";
-import { useEffect, useState } from "react";
 
-const NFCReader: React.FC = () => {
-  const [nfcData, setNfcData] = useState<string | null>(null);
-  const [error, setError] = useState<string | null>(null);
+import { useState } from "react";
 
-  useEffect(() => {
-    const handleNFCReading = async () => {
-      if ("NDEFReader" in window) {
-        const ndef = new (window as any).NDEFReader();
-
-        try {
-          await ndef.scan();
-          console.log("Scan started successfully.");
-
-          ndef.onreadingerror = () => {
-            setError("Error reading NFC tag.");
-          };
-
-          ndef.onreading = (event: any) => {
-            const decoder = new TextDecoder();
-            for (const record of event.message.records) {
-              console.log("Record type:", record.recordType);
-              console.log("MIME type:", record.mediaType);
-              console.log("=== data ===\n" + decoder.decode(record.data));
-              setNfcData(decoder.decode(record.data));
-            }
-          };
-        } catch (error) {
-          setError(`Error: ${error}`);
-        }
-      } else {
-        setError("Web NFC is not supported on this browser.");
-      }
-    };
-
-    handleNFCReading();
-  }, []);
-
-  return (
-    <div>
-      <h1>NFC Reader</h1>
-      {error && <p>Error: {error}</p>}
-      {nfcData ? <p>NFC Data: {nfcData}</p> : <p>No NFC data read yet.</p>}
-    </div>
-  );
+const Scanning = () => {
+  const [text, setText] = useState<null | string>(null);
+  if ("NDEFReader" in window) {
+    setText("hello bro");
+  }
+  return <div>{text}</div>;
 };
 
-export default NFCReader;
+export default Scanning;
